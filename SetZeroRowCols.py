7a2f7628c619b8ea5202309f86b4f27339c27c9c
Sandeep Sharma <sandeep16sharma0499@gmail.com>
2021-04-12 19:12:01 +0530

    Update and rename SetZeroRowCols to SetZeroRowCols.py
    Set Zero Row col using numpy array MxN matrix

--- SetZeroRowCols.py
+++ SetZeroRowCols.py
@@ -1,5 +1,5 @@
 #	   Problem Statement:
-#	   The input is a N x N matrix where some of the items in the matrix are 0.
+#	   The input is a M x N(Row x Col) matrix where some of the items in the matrix are 0.
 #  	 If any of the items are 0, then set that item's entire row and column to 0.
 #	   items that are not 0 will be represented by an integer.
 
